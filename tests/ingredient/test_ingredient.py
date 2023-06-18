b556f28135a15d63203d437cec29de2d69ee076d
Victor Cabral <victor_cabral_o@hotmail.com>
2023-06-18 17:44:32 -0300

    test: tests for ingredient

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -3,4 +3,11 @@ from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
 # Req 1
 def test_ingredient():
-    pass
+    cheese_ingredient = Ingredient("queijo mussarela")
+    water_ingredient = Ingredient("água")
+
+    assert water_ingredient.__hash__() == hash(water_ingredient.name)
+    assert water_ingredient.__eq__(water_ingredient) == True
+    assert water_ingredient.__eq__(cheese_ingredient) == False
+    assert water_ingredient.__repr__() == "Ingredient('água')"
+    assert len(cheese_ingredient.restrictions) == 2
